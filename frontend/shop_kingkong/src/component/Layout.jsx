5e90e1eab7a56176737b760bc2aa31ecfc89a52d
tzanlam <tzanlam@gmail.com>
2025-09-10 18:05:48 +0700

    delete comment

--- frontend/shop_kingkong/src/component/Layout.jsx
+++ frontend/shop_kingkong/src/component/Layout.jsx
@@ -7,16 +7,10 @@ import Footer from "./Footer";
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Header cố định */}
       <Header />
-
-      {/* Nội dung chính với padding */}
       <main className="flex-grow pb-20">
         <Outlet />{" "}
-        {/* Các trang như HomePage, ProductDetails sẽ được render ở đây */}
       </main>
-
-      {/* Footer cố định */}
       <Footer />
     </div>
   );
